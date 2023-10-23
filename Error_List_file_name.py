ae5a95dd0286432b515c0dee660fd9281978ae10
Ariful Islam <48016185+ariful228@users.noreply.github.com>
2023-10-23 13:42:41 +0600

    list file name and add in a row with separator OR for searching vault

--- Error_List_file_name.py
+++ Error_List_file_name.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
+#List down column
 # Read the Excel file into a DataFrame
 excel_file = r"D:\PYDATAANALYSIS\Error_List_file_name copy.xlsx"
 df = pd.read_excel(excel_file)
 
-# List to store the extracted last parts
-last_parts = []
+# List to store the concatenated values
+concatenated_values = []
 
 # Process and check all rows in the DataFrame
 for index, row in df.iterrows():
@@ -19,13 +20,31 @@ for index, row in df.iterrows():
         parts = column_value.split("\\")
         if len(parts) > 0:
             last_part = parts[-1]
-            last_parts.append(last_part)
+            concatenated_values.append(last_part)
 
-# Create a DataFrame with the extracted last parts
-last_parts_df = pd.DataFrame(last_parts, columns=["Last Part"])
+# Create a DataFrame with the concatenated values
+concatenated_df = pd.DataFrame(concatenated_values, columns=["Concatenated"])
 
 # Export the DataFrame to another file
-exported_excel_file = "exported_last_parts.xlsx"
-last_parts_df.to_excel(exported_excel_file, index=False)
+exported_excel_file = "exported_concatenated_values.xlsx"
+concatenated_df.to_excel(exported_excel_file, index=False)
 
-print(f"Last parts exported to {exported_excel_file}")
+print(f"Concatenated values exported to {exported_excel_file}")
+
+
+#ROW
+# Read the Excel file into a DataFrame
+excel_file = r"D:\PYDATAANALYSIS\exported_concatenated_values.xlsx"
+
+# Create a new DataFrame with the formatted string as the only row
+new_row = pd.DataFrame({'Formatted': [df['Concatenated'].str.cat(sep=' OR ')]})
+
+# Define the sheet name for the new row
+sheet_name = 'NewSheet'
+
+# Open the Excel file and write the new row to the specified sheet
+with pd.ExcelWriter(excel_file, engine='openpyxl', mode='a') as writer:
+    new_row.to_excel(writer, sheet_name=sheet_name, index=False)
+
+print(f"Formatted value added to sheet '{sheet_name}' in {excel_file}")
+
